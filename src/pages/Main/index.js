b4000710bcb106877b3022cdec3f8e11a49b317e
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-05-13 12:18:26 -0300

    github api resquest

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Keyboard } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import api from '../../services/api'
+
 import { Container, Form, Input, SubmitButton } from './styles'
 
-const index = () => {
-    return (
-        <Container>
-            <Form>
-                <Input
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                    placeholder="Adicionar um usuário"
-                />
-                <SubmitButton>
-                    <Icon name="add" size={20} color="#fff" />
-                </SubmitButton>
-            </Form>
-        </Container>
-    )
+export default class Main extends Component {
+    state = {
+        newUser: '',
+        users: []
+    }
+
+    handleAddUser = async () => {
+        const { users, newUser } = this.state
+
+        const response = await api.get(`/users/${newUser}`)
+
+        const data = {
+            name: response.data.name,
+            login: response.data.login,
+            bio: response.data.bio,
+            avatar: response.data.avatar
+        }
+
+        this.setState({
+            users: [...users, data],
+            newUser: ''
+        })
+
+        Keyboard.dismiss()
+
+    }
+
+    render() {
+        const { user, newUser } = this.state
+
+        return (
+            <Container>
+                <Form>
+                    <Input
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Adicionar um usuário"
+                        value={newUser}
+                        onChangeText={text => this.setState({ newUser: text })}
+                        returnKeyType='send'
+                        onSubmitEditing={this.handleAddUser}
+                    />
+                    <SubmitButton onPress={this.handleAddUser}>
+                        <Icon name="add" size={20} color="#fff" />
+                    </SubmitButton>
+                </Form>
+            </Container >
+        )
+    }
 }
 
-export default index
+
