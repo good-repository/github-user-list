d0f4d9556ea6dbbe87fcc21719d33da084d35130
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-05-13 15:31:33 -0300

    styling list

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,7 +4,6 @@ import { RectButton } from 'react-native-gesture-handler'
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
-  background: #333;
 `;
 
 
@@ -20,7 +19,7 @@ export const Input = styled.TextInput.attrs({
 })`
   flex: 1;
   height: 40px;
-  background: #eee;
+  background: lightgray;
   border-radius: 4px;
   padding: 0 15px;
   border: 1px solid #eee;
@@ -33,4 +32,55 @@ export const SubmitButton = styled(RectButton)`
   border-radius: 4px;
   margin-left: 10px;
   padding: 0 12px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false
+})`
+  margin-top: 20px;
+`;
+
+export const User = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+`;
+
+export const Avatar = styled.Image`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  background: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 14px;
+  color: #333;
+  font-weight: bold;
+  margin-top: 4px;
+  text-align: center;
+`;
+
+export const Bio = styled.Text`
+  font-size: 13px;
+  line-height: 18px;
+  color: #999;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const ProfileButton = styled(RectButton)`
+  margin-top: 10px;
+  align-self: stretch;
+  border-radius: 4px;
+  background: #7159c1;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+`;
+
+export const ProfileButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
 `;
